26621e239e0d371d88a3ab0ec297f7626a081add
ptyszko <tyszko@student.agh.edu.pl>
2020-10-19 17:47:27 +0200

    minimal lexer satisfying the conditions

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import sys
+import ply.lex as lex
+# scanner.py is a file you create, (it is not an external library)
 import scanner
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     try:
         filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
         file = open(filename, "r")
     except IOError:
-        print("Cannot open {0} file".format(filename))
+        print("Cannot open file: {0}".format(filename))
         sys.exit(0)
 
     text = file.read()
-    scanner.source = text
     lexer = scanner.lexer
     lexer.input(text)  # Give the lexer some input
 
     # Tokenize
     while True:
         tok = lexer.token()
-        if not tok:
-            break  # No more input
-        column = scanner.find_column(tok)
-        print("(%d,%d): %s(%s)" % (tok.lineno, column, tok.type, tok.value))
+        if tok is None:
+            break    # No more input
+        #column = scanner.find_column(text,tok)
+        print("({0:d}): {1}({2})".format(tok.lineno, tok.type, tok.value))
--- scanner.py
+++ scanner.py
@@ -1,138 +1,71 @@
-#!/usr/bin/env python
+from ply.lex import lex, Lexer
 
-import ply.lex as lex
 
 reserved = {
-    'if': 'IF',
-    'then': 'THEN',
-    'else': 'ELSE',
-    'while': 'WHILE',
+    'if': 'IF', 'else': 'ELSE', 'while': 'WHILE', 'if': 'IF',
+    'break': 'BREAK', 'continue': 'CONTINUE', 'return': 'RETURN',
+    'eye': 'EYE', 'zeros': 'ZEROS', 'ones': 'ONES', 'print': 'PRINT'
 }
 
-tokens = \
-    ['ID',
-     'FloatingPointNumber',
-     'IntegerNumber',
-     'String',
-     # binary operators
-     'Plus',
-     'Minus',
-     'Multiply',
-     'Divide',
-     # matrix binary operators
-     'MatBinaryPlus',
-     'MatrixBinaryMinus',
-     'MatrixBinaryMultiply',
-     'MatrixBinaryDivide',
-     # assign operators
-     'Assign',
-     'AddAndAssign',
-     'SubtractAndAssign',
-     'MultiplyAndAssign',
-     'DivideAndAssign',
-     # relational operators
-     'LesserThan',
-     'GreaterThan',
-     'LesserOrEqualThan',
-     'GreaterOrEqualThan',
-     'NotEqual',
-     'Equal',
-     # brackets
-     'RoundBracketLeft',
-     'RoundBracketRight',
-     'CurlyBracketLeft',
-     'CurlyBracketRight',
-     'SquareBracketLeft',
-     'SquareBracketRight',
-     # various lexems
-     'RangeOperator',
-     'MatrixTransposition',
-     'Comma',
-     'Semicolon',
-     'Comment'
-     ] + list(reserved.values())
+
+tokens = (
+    'DOTADD', 'DOTSUB', 'DOTMUL', 'DOTDIV', 'ADDASSIGN', 'SUBASSIGN',
+    'MULASSIGN', 'DIVASSIGN', 'LE', 'GE', 'EQ', 'NE', 'ID', 'INT',
+    'FLOAT', 'STR'
+) + tuple(reserved.values())
 
 
-t_String = r'".*"'
-# binary operators
-t_Plus = r'\+'
-t_Minus = r'-'
-t_Multiply = r'\*'
-t_Divide = r'/'
-# matrix binary operators
-t_MatBinaryPlus = r'\.\+'
-t_MatrixBinaryMinus = r'\.-'
-t_MatrixBinaryMultiply = r'\.\*'
-t_MatrixBinaryDivide = r'\./'
-# assign operators
-t_Assign = r'='
-t_AddAndAssign = r'\+='
-t_SubtractAndAssign = r'-='
-t_MultiplyAndAssign = r'\*='
-t_DivideAndAssign = r'/='
-# relational operators
-t_LesserThan = r'<'
-t_GreaterThan = r'>'
-t_LesserOrEqualThan = r'<='
-t_GreaterOrEqualThan = r'>='
-t_NotEqual = r'!='
-t_Equal = r'=='
-# brackets
-t_RoundBracketLeft = r'\('
-t_RoundBracketRight = r'\)'
-t_CurlyBracketLeft = r'\{'
-t_CurlyBracketRight = r'\}'
-t_SquareBracketLeft = r'\['
-t_SquareBracketRight = r'\]'
-# various lexems
-t_RangeOperator = r':'
-t_MatrixTransposition = r"'"
-t_Comma = r','
-t_Semicolon = r';'
-t_ignore_Comment = r'\#.*'
+t_DOTADD = r'\.\+'
+t_DOTSUB = r'\.-'
+t_DOTMUL = r'\.\*'
+t_DOTDIV = r'\./'
+t_ADDASSIGN = r'\+='
+t_SUBASSIGN = r'-='
+t_MULASSIGN = r'\*='
+t_DIVASSIGN = r'/='
+t_LE = r'<='
+t_GE = r'>='
+t_EQ = r'=='
+t_NE = r'\!='
 
+literals = '+-*/=()[]{}<>:\',;'
 t_ignore = ' \t'
+t_ignore_COMMENT = r'\#.*'
 
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
+    t.type = reserved.get(t.value, 'ID')    # Check for reserved words
     return t
 
 
-# it's must be before t_IntegerNumber, otherwise lexer would interpret e.g. 60. like integer(60) and invalid symbol (.)
-def t_FloatingPointNumber(t):
-    r'(\d*\.\d+)|(\d+\.\d*)' # a.x or x.a where a could be empty
+def t_FLOAT(t):
+    r'(\d+\.\d*)|(\d*\.\d+)'
     t.value = float(t.value)
     return t
 
 
-def t_IntegerNumber(t):
+def t_INT(t):
     r'\d+'
     t.value = int(t.value)
+    return t
+
+
+def t_STR(t):
+    r'"([^"]|\\")*"'
+    t.value = t.value[1:-1]
     return t
 
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+    t.lexer.last_newline = t.lexpos
 
 
 def t_error(t):
-    print("(%d, %d): illegal character '%s'" % (t.lineno, find_column(t), t.value[0]))
+    print("Illegal character '{}'".format(t.value[0]))
     t.lexer.skip(1)
 
 
-lexer = lex.lex()
-source = ""
-
-
-# Compute column.
-# input is the input text string
-# token is a token instance
-def find_column(token_to_find):
-    last_cr = source.rfind('\n', 0, token_to_find.lexpos)
-    if last_cr < 0:
-        last_cr = 0
-    column = (token_to_find.lexpos - last_cr)
-    return column
+lexer = lex()
